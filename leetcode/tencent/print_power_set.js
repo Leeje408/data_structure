e91add1b80584386843c16c03a8c584e6d44ae28
liziyu <leeje408@gmail.com>
2017-08-28 02:22:24 +0800

    打印子集方法原理注释

--- leetcode/tencent/print_power_set.js
+++ leetcode/tencent/print_power_set.js
@@ -20,6 +20,17 @@ function powerSet(array, index, result) {
     }
 }
 
+function printPowerSet(array) {
+    /**
+     * 使用二进制位表示元素，0不打印，1则打印
+     * e.g {1,2,3}
+     * 二进制为 111 则打印 123
+     * 二进制位 001 则打印 1
+     * 此处二进制第一位表示为元素在集合中的位置
+     */
+    
+}
+
 function _test_(array) {
     console.log('result:' + JSON.stringify(powerSet(array)))
 }
